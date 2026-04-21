4af3e1ef87f44a3f987077c4cc15b0d018016050
Yasiru-Sanjana <yasirusanjana51@gmail.com>
2026-04-21 16:49:10 +0530

    updated the basic setup in SensorReading class

--- src/main/java/SmartCampusApplication/model/SensorReading.java
+++ src/main/java/SmartCampusApplication/model/SensorReading.java
@@ -9,5 +9,39 @@ package SmartCampusApplication.model;
  * @author yasirusanjana
  */
 public class SensorReading {
+    private String id;          // Unique reading event ID ( UUIDrecommended )
+    private long timestamp ;    // Epoch time (ms) when the reading was captured
+    private double value ;      // The actual metric value recorded by the hardware
+
+    public SensorReading(String id, long timestamp, double value) {
+        this.id = id;
+        this.timestamp = timestamp;
+        this.value = value;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+    
     
 }
