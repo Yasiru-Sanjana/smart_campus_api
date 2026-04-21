f454084c168743399105892a8a5073a9aac81155
Yasiru-Sanjana <yasirusanjana51@gmail.com>
2026-04-21 16:47:42 +0530

    Constructors, getters, setters are added to the main POJO classes

--- src/main/java/SmartCampusApplication/model/Room.java
+++ src/main/java/SmartCampusApplication/model/Room.java
@@ -16,6 +16,38 @@ public class Room {
     private String name;            // Human - readable name , e.g.," Library
     private int capacity;           // Maximum occupancy for safety
     private List<String> sensorIds = new ArrayList<>();     // Collection of IDs of sensors deployed in this room
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public List<String> getSensorIds() {
+        return sensorIds;
+    }
+
+    public void addSensorId(String sensorId) {
+        this.sensorIds.add(sensorId);
+    }
     
     
     
