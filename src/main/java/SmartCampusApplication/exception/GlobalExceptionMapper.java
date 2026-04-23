aeac5503f7887ff7cc60e4ef79833d99dd51585a
Yasiru-Sanjana <yasirusanjana51@gmail.com>
2026-04-23 15:51:58 +0530

    Code complete and ready for testing

--- src/main/java/SmartCampusApplication/exception/GlobalExceptionMapper.java
+++ src/main/java/SmartCampusApplication/exception/GlobalExceptionMapper.java
@@ -6,10 +6,29 @@ package SmartCampusApplication.exception;
 
 import SmartCampusApplication.model.ErrorMessage;
 import javax.ws.rs.core.*;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
 /**
  *
  * @author yasirusanjana
  */
-public class GlobalExceptionMapper {
-    
+@Provider
+public class GlobalExceptionMapper implements ExceptionMapper<Throwable> {
+
+    @Override
+    public Response toResponse(Throwable exception) {
+        //log the error
+        exception.printStackTrace(); 
+
+        // Send a general messege to the server
+        ErrorMessage error = new ErrorMessage(
+            "An unexpected internal server error occurred. Please try again later.", 
+            500
+        );
+
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                       .entity(error)
+                       .type(MediaType.APPLICATION_JSON)
+                       .build();
+    }
 }
